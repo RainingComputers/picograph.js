fda920441629461d959c89e32bd6c213963a3808
RainingComputers <vishnu.vish.shankar@gmail.com>
2021-02-05 07:16:10 +0530

    Refactored code

--- picograph.js
+++ picograph.js
@@ -5,40 +5,38 @@
     https://github.com/RainingComputers
 */
 
-colors = [
+const colors = [
     "#e52b50", "#008000", "#0000ff", "#ff00ff",
-    "#a52a2a", "#00008b", "#008b8b", "#a9a9a9", 
-    "#006400", "#bdb76b", "#8b008b", "#556b2f", 
-    "#ff8c00", "#9932cc", "#8b0000", "#e9967a", 
-    "#9400d3", "#ff00ff", "#ffd700", "#008000", 
-    "#4b0082", "#f0e68c", "#add8e6", "#e0ffff", 
-    "#90ee90", "#d3d3d3", "#ffb6c1", "#ffffe0", 
-    "#800000", "#000080", "#808000", "#ffa500", 
-    "#ffc0cb", "#800080", "#800080", "#ff0000", 
+    "#a52a2a", "#00008b", "#008b8b", "#a9a9a9",
+    "#006400", "#bdb76b", "#8b008b", "#556b2f",
+    "#ff8c00", "#9932cc", "#8b0000", "#e9967a",
+    "#9400d3", "#ff00ff", "#ffd700", "#008000",
+    "#4b0082", "#f0e68c", "#add8e6", "#e0ffff",
+    "#90ee90", "#d3d3d3", "#ffb6c1", "#ffffe0",
+    "#800000", "#000080", "#808000", "#ffa500",
+    "#ffc0cb", "#800080", "#800080", "#ff0000",
     "#c0c0c0", "#ffffff", "#ffff00"
 ]
 
-const byID = function(id) { return document.getElementById(id); };
+const byID = function (id) { return document.getElementById(id); };
 
 /* Helper function for creating graphs */
-function createGraph(canvasID, labels, unit, labelDivID, intervalSize, maxVal, 
-    vlines=false, timestamps=false, scalesteps=5)
-{
+function createGraph(canvasID, labels, unit, labelDivID, intervalSize, maxVal,
+    vlines = false, timestamps = false, scalesteps = 5) {
     /* Create valueIDs for each label */
-    valueIDs = []
-    for(let i = 0; i < labels.length; i++) {
+    const valueIDs = []
+    for (let i = 0; i < labels.length; i++) {
         valueIDs[i] = canvasID + labels[i].replace(" ", "") + "value";
     }
 
     /* Create graph  */
-    const graph = new Graph(canvasID, labels.length, valueIDs, unit, intervalSize, maxVal, 
+    const graph = new Graph(canvasID, labels.length, valueIDs, unit, intervalSize, maxVal,
         vlines, timestamps, scalesteps);
 
     /* Set label colors */
-    for(let i = 0; i < labels.length; i++)
-    {
+    for (let i = 0; i < labels.length; i++) {
         const colorID = valueIDs[i] + "color";
-        
+
         byID(labelDivID).innerHTML += `
             <div style="display: inline-block;">
                 <svg width="10" height="10">
@@ -49,23 +47,21 @@ function createGraph(canvasID, labels, unit, labelDivID, intervalSize, maxVal,
             <div>
         `
 
-        labelcolor = graph.colors[i];
-        byID(colorID).style = "fill:"+labelcolor;
+        const labelcolor = graph.colors[i];
+        byID(colorID).style = "fill:" + labelcolor;
     }
 
     return graph;
 }
 
 /* Graph class, plots and updates graphs */
-class Graph
-{
-    constructor(canvasID, noLabels, valueIDs, unit, intervalSize, maxVal, vlines, timestamps, scalesteps)
-    {
+class Graph {
+    constructor(canvasID, noLabels, valueIDs, unit, intervalSize, maxVal, vlines, timestamps, scalesteps) {
         /* Get the drawing context */
         this.canvas = byID(canvasID);
         const ctx = this.canvas.getContext("2d");
         this.ctx = ctx;
-        
+
         /* Set proper height and width */
         this.setWidthHeight()
 
@@ -73,9 +69,9 @@ class Graph
         this.cssScale = window.devicePixelRatio;
         this.scalesteps = scalesteps
         this.noLabels = noLabels;
-        this.intervalSize = intervalSize*this.cssScale;
-        this.nValuesFloat = this.width/this.intervalSize
-        this.nValues = Math.round(this.nValuesFloat)+1;
+        this.intervalSize = intervalSize * this.cssScale;
+        this.nValuesFloat = this.width / this.intervalSize
+        this.nValues = Math.round(this.nValuesFloat) + 1;
         this.points = emptyArray(noLabels, this.nValues);
         this.timestamps_array = emptyArray(1, this.nValues, "");
         this.colors = colorArray(noLabels);
@@ -86,22 +82,20 @@ class Graph
         this.timestamps = timestamps;
     }
 
-    setWidthHeight()
-    {
+    setWidthHeight() {
         this.cssScale = window.devicePixelRatio;
 
         /* Set canvas height and width */
-        this.canvas.width = this.canvas.clientWidth*this.cssScale;
-        this.canvas.height = this.canvas.clientHeight*this.cssScale;
+        this.canvas.width = this.canvas.clientWidth * this.cssScale;
+        this.canvas.height = this.canvas.clientHeight * this.cssScale;
         this.width = this.ctx.canvas.width;
         this.height = this.ctx.canvas.height;
     }
 
-    update(values)
-    {
-        for(let i = 0; i < this.noLabels; i++) {
+    update(values) {
+        for (let i = 0; i < this.noLabels; i++) {
             /* Update scale */
-            if(values[i] > this.maxVal) {
+            if (values[i] > this.maxVal) {
                 this.maxVal = values[i];
             }
 
@@ -109,12 +103,12 @@ class Graph
             this.points = shiftArrayRowLeft(this.points, i, this.nValues, values[i]);
 
             /* Update value */
-            byID(this.valueIDs[i]).innerHTML = values[i].toFixed(2)+' '+this.unit;
-        } 
+            byID(this.valueIDs[i]).innerHTML = values[i].toFixed(2) + ' ' + this.unit;
+        }
 
         /* Log time and add to timestamps_array array */
         const d = new Date();
-        const timestamp_str = d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+        const timestamp_str = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
         this.timestamps_array = shiftArrayRowLeft(this.timestamps_array, 0, this.nValues, timestamp_str);
 
         /* Clear canvas */
@@ -124,82 +118,75 @@ class Graph
         this.setWidthHeight()
 
         /* update interval size */
-        this.intervalSize = this.width/this.nValuesFloat;
+        this.intervalSize = this.width / this.nValuesFloat;
 
         /* Set line width */
-        this.ctx.lineWidth = 2*this.cssScale;
+        this.ctx.lineWidth = 2 * this.cssScale;
 
         /* Draw vertical scale */
-        if(this.vlines)
-        {
-            for(let i = this.nValues-1; i >= 0; i--)
-            {
+        if (this.vlines) {
+            for (let i = this.nValues - 1; i >= 0; i--) {
                 /* Calculate line coordinates */
-                const x = (i+1)*this.intervalSize;
-    
+                const x = (i + 1) * this.intervalSize;
+
                 /* Draw line */
                 this.ctx.beginPath();
                 this.ctx.moveTo(x, 0);
                 this.ctx.lineTo(x, this.height);
                 this.ctx.strokeStyle = "#e3e3e3";
                 this.ctx.stroke();
-            }   
+            }
         }
 
         /* Calculate font size and space between scale lines */
-        const hstep = this.height/this.scalesteps;
-        const sstep = this.maxVal/this.scalesteps;
+        const hstep = this.height / this.scalesteps;
+        const sstep = this.maxVal / this.scalesteps;
 
-        const canvas_font = Math.min(0.5*hstep, 15*this.cssScale)
-        this.ctx.font = canvas_font+"px monospace";
-        
-        
+        const canvas_font = Math.min(0.5 * hstep, 15 * this.cssScale)
+        this.ctx.font = canvas_font + "px monospace";
+
+
         /* Draw horizontal scale */
-        for(let i = 1; i <= this.scalesteps; i++)
-        {
-            const y = this.height-i*hstep
+        for (let i = 1; i <= this.scalesteps; i++) {
+            const y = this.height - i * hstep
             const xoffset = 2;
-            const yoffset = canvas_font+2*this.cssScale;
-            this.ctx.fillText((i*sstep).toFixed(2), xoffset, y+yoffset);
+            const yoffset = canvas_font + 2 * this.cssScale;
+            this.ctx.fillText((i * sstep).toFixed(2), xoffset, y + yoffset);
             this.ctx.beginPath();
             this.ctx.moveTo(0, y);
             this.ctx.lineTo(this.width, y);
             this.ctx.strokeStyle = "#e3e3e3";
             this.ctx.stroke();
         }
-        
+
         /* Draw time stamps */
-        if(this.timestamps)
-        {
-            const xBoundPix = this.ctx.measureText((this.scalesteps*sstep).toFixed(2)).width;
-            const xBound = Math.floor((xBoundPix/this.intervalSize)+1)
-            for(let i = this.nValues-1; i >= xBound; i--)
-            {
+        if (this.timestamps) {
+            const xBoundPix = this.ctx.measureText((this.scalesteps * sstep).toFixed(2)).width;
+            const xBound = Math.floor((xBoundPix / this.intervalSize) + 1)
+            for (let i = this.nValues - 1; i >= xBound; i--) {
                 /* Calculate line coordinates */
-                const x = (i+1)*this.intervalSize;
-    
-    
+                const x = (i + 1) * this.intervalSize;
+
+
                 /* Put time stamps */
-                const xoffset = canvas_font+2*this.cssScale;
-                const yoffset = this.ctx.measureText(this.timestamps_array[0][i]).width+4*this.cssScale;
-                this.ctx.rotate(Math.PI/2);
-                this.ctx.fillText(this.timestamps_array[0][i], this.height-yoffset, -x+xoffset);
+                const xoffset = canvas_font + 2 * this.cssScale;
+                const yoffset = this.ctx.measureText(this.timestamps_array[0][i]).width + 4 * this.cssScale;
+                this.ctx.rotate(Math.PI / 2);
+                this.ctx.fillText(this.timestamps_array[0][i], this.height - yoffset, -x + xoffset);
                 this.ctx.stroke();
-                this.ctx.rotate(-Math.PI/2);
-            }               
-        }  
+                this.ctx.rotate(-Math.PI / 2);
+            }
+        }
 
         /* Draw graph */
-        for(let i = 0; i < this.noLabels; i++)
-        {
-            for(let j = this.nValues-1; j > 0; j--)
-            {
+        for (let i = 0; i < this.noLabels; i++) {
+            for (let j = this.nValues - 1; j > 0; j--) {
                 /* Calculate line coordinates */
-                const xstart = (j+1)*this.intervalSize;
-                const xend = j*this.intervalSize;
+                const xstart = (j + 1) * this.intervalSize;
+                const xend = j * this.intervalSize;
                 const ystart = scaleInvert(this.points[i][j], this.maxVal, this.height);
-                const yend = scaleInvert(this.points[i][j-1], this.maxVal, this.height);
-                
+                const yend = scaleInvert(this.points[i][j - 1], this.maxVal, this.height);
+
                 /* Draw line */
                 this.ctx.beginPath();
                 this.ctx.moveTo(xstart, ystart);
@@ -215,30 +202,27 @@ class Graph
 
 /* Helper function to take a value value 
     and return y-coordinate for the canvas */
-function scaleInvert(value, maxVal, height)
-{
-    return (1 - value/maxVal) * height;
+function scaleInvert(value, maxVal, height) {
+    return (1 - value / maxVal) * height;
 }
 
 /* Helper function that shifts the contents of row to left */
-function shiftArrayRowLeft(array, row, ncols, newVal)
-{
-    for(let i = 0; i < ncols-1; i++) {
-        array[row][i] = array[row][i+1];
+function shiftArrayRowLeft(array, row, ncols, newVal) {
+    for (let i = 0; i < ncols - 1; i++) {
+        array[row][i] = array[row][i + 1];
     }
 
-    array[row][ncols-1] = newVal;
+    array[row][ncols - 1] = newVal;
 
     return array;
 }
 
 /* Helper function to create an empty */
-function emptyArray(nrows, ncols, fill=0) 
-{
+function emptyArray(nrows, ncols, fill = 0) {
     const arr = [];
-    for(let i = 0; i < nrows; i++) {
+    for (let i = 0; i < nrows; i++) {
         arr[i] = [];
-        for(let j = 0; j < ncols; j++) {
+        for (let j = 0; j < ncols; j++) {
             arr[i][j] = fill;
         }
     }
@@ -246,12 +230,10 @@ function emptyArray(nrows, ncols, fill=0)
 }
 
 /* Helper function to create array of colors */
-function colorArray(len)
-{
+function colorArray(len) {
     const colorArray = [];
-    for(let i = 0; i < len; i++)
-    {
-        colorArray[i] = colors[i%colors.length];
+    for (let i = 0; i < len; i++) {
+        colorArray[i] = colors[i % colors.length];
     }
     return colorArray;
 }
